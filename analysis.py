5fa3020d1dd7295bb162e617579dda62099a838a
Joe <joe@MacBook-Air-2.local>
2017-09-19 18:51:39 -0700

    added some final functionality to analysis

--- analysis.py
+++ analysis.py
@@ -68,7 +68,34 @@ def add_strat_col(df, zerg, protoss, terran, zerg_strat, protoss_strat, terran_s
     df['Strategy'] = col
     return df
 
-def get_mu(df):
+def get_race_mu(df):
+    num_games = (df.shape[0]+1)/2
+    results = []
+    matchup_totals = defaultdict(int)
+    for game_num in xrange(num_games):
+        player1 = df.loc[2*game_num]
+        player2 = df.loc[2*game_num+1]
+        p1race = player1['race']
+        p2race = player2['race']
+        if p1race != p2race: #remove mirror matches
+            matchup = ' vs '.join(sorted([p1race, p2race]))
+            matchup_totals[matchup]+=1
+            if player1['Won']:
+                result = (matchup, p1race)
+            else:
+                result = (matchup, p2race)
+            results.append(result)
+    return results, matchup_totals
+
+def mu_win_percent(df):
+    results, matchup_totals = get_race_mu(df)
+    results_count = Counter(results)
+    result =  {(matchup, winner):((float(count)/matchup_totals[matchup]), matchup_totals[matchup]) for (matchup, winner), count in results_count.iteritems()}
+    result_list = list(result.items())
+    result_sorted = sorted(result_list, key= lambda x: x[1][0], reverse=True)
+    return result_sorted
+
+def get_race_strat_mu(df):
     num_games = (df.shape[0]+1)/2
     results = []
     matchup_totals = defaultdict(int)
@@ -87,18 +114,28 @@ def get_mu(df):
             results.append(result)
     return results, matchup_totals
 
-def mu_win_percent(df):
-    results, matchup_totals = get_mu(df)
+def mu_strategy_win_percent(df):
+    results, matchup_totals = get_race_strat_mu(df)
     results_count = Counter(results)
-    result =  {(matchup, winner):((float(count)/matchup_totals[matchup]), matchup_totals[matchup]) for (matchup, winner), count in results_count.iteritems() if ztest_prob((float(count)/matchup_totals[matchup]), matchup_totals[matchup])>.95 and matchup_totals[matchup]>5}
+    result =  {(matchup, winner):((float(count)/matchup_totals[matchup]), matchup_totals[matchup]) for (matchup, winner), count in results_count.iteritems()}
     result_list = list(result.items())
     result_sorted = sorted(result_list, key= lambda x: x[1][0], reverse=True)
     return result_sorted
 
+    '''if ztest_prob((float(count)/matchup_totals[matchup]), matchup_totals[matchup])>.95 and matchup_totals[matchup]>5'''
+
 def ztest_prob(win_percent, matchup_total):
     return proportions_ztest(win_percent*matchup_total, matchup_total, value=.5, alternative='smaller')[1]
 
-def print_matchup_results(df):
+def print_strategy_matchup_results(df):
+    results = mu_strategy_win_percent(df)
+    for result in results:
+        print "Matchup: ", result[0][0]
+        print "Winner: ", result[0][1]
+        print "Won {}% of {} games".format(100*result[1][0], result[1][1])
+        print "----------------------"
+
+def print_race_matchup_results(df):
     results = mu_win_percent(df)
     for result in results:
         print "Matchup: ", result[0][0]
@@ -107,7 +144,10 @@ def print_matchup_results(df):
         print "----------------------"
 
 def get_strat_pair_counts(df):
-    return df.groupby(['race', 'Strategy Pairs']).count()
+    return df.groupby(['race', 'Strategy Pairs']).size()
+
+def gamelength_per_strat(df):
+    return df.groupby(['race', 'Strategy'])['gamelength'].mean().divide(480)
 
 
 #Zerg: 4 strats
@@ -148,7 +188,7 @@ Counts of each strat:
 2    1079
 3     451
 
-note that stratx is labelled as x-1
+
 '''
 
 #Terran: 5
@@ -197,13 +237,13 @@ strat counts:
 3    1664
 4     169
 
-note that this is with strat1 dropped and stratx is labelled as x-1
+note that this is with strat0 dropped
 '''
 
 #Protoss: 6 strats
 #drop Terran_SCV
 '''
-protoss6:
+protoss:
 ** drop strat0
 Protoss_0: Baseline required units
 Protoss_Probe          7.687033
@@ -219,7 +259,7 @@ Protoss_Gateway        0.711189
 Protoss_Observer       0.528759
 Protoss_Observatory    0.213965
 
-Protoss_2: zealot rush
+Protoss_2: zealots
 Protoss_Zealot          5.771848
 Protoss_Pylon           0.902310
 Protoss_Gateway         0.627979
@@ -254,8 +294,42 @@ strat counts:
 5     269
 3      53
 
-note that this is with strat1 dropped and stratx is labelled as x-1
+note that this is with strat0 dropped
 '''
+
+'''race matchups
+Matchup:  Protoss vs Zerg
+Winner:  Zerg
+Won 52.9045643154% of 5784 games
+----------------------
+Matchup:  Terran vs Zerg
+Winner:  Terran
+Won 51.4611087237% of 4654 games
+----------------------
+Matchup:  Protoss vs Terran
+Winner:  Protoss
+Won 50.0619578686% of 5649 games
+'''
+
+'''
+overall win rates per strategy:
+
+race     Strategy     winrate
+Protoss  1.0         0.499455
+         2.0         0.487384
+         3.0         0.679245
+         4.0         0.318982
+         5.0         0.490706
+Terran   1.0         0.475000
+         2.0         0.552525
+         3.0         0.503606
+         4.0         0.497041
+Zerg     0.0         0.494550
+         1.0         0.513207
+         2.0         0.521779
+         3.0         0.547672
+'''
+
 
 
 '''
@@ -282,46 +356,154 @@ most unbalanced win rates:
  (('Protoss_1Protoss_2', 'Protoss_1'), (0.5678496868475992, 479)),
  (('Protoss_1Terran_2', 'Terran_2'), (0.5475398936170213, 3008)),
  (('Terran_1Zerg_0', 'Terran_1'), (0.5319303338171263, 1378))
+
+with strategy descriptions:
+
+Matchup:  Terran: Goliaths, heavy air defense vs Terran: Vultures
+Winner:  Terran: Vultures
+Won 100.0% of 6 games
+----------------------
+Matchup:  Terran: Goliaths, heavy air defense vs Terran: siege tanks
+Winner:  Terran: siege tanks
+Won 90.9090909091% of 11 games
+----------------------
+Matchup:  Protoss: reaver drop vs Terran: Vultures
+Winner:  Protoss: reaver drop
+Won 87.5% of 8 games
+----------------------
+Matchup:  Protoss: dark templar rush vs Terran: Marine/Medic combo
+Winner:  Protoss: dark templar rush
+Won 87.0967741935% of 31 games
+----------------------
+Matchup:  Protoss: reaver drop vs Zerg: building defense
+Winner:  Protoss: reaver drop
+Won 76.4705882353% of 17 games
+----------------------
+Matchup:  Terran: Goliaths, heavy air defense vs Zerg: hydra/lurker
+Winner:  Terran: Goliaths, heavy air defense
+Won 72.7272727273% of 11 games
+----------------------
+Matchup:  Protoss: Cannon rush or Cannon defense + Corsairs vs Zerg: zergling rush
+Winner:  Zerg: zergling rush
+Won 71.4285714286% of 175 games
+----------------------
+Matchup:  Terran: Marine/Medic combo vs Terran: Vultures
+Winner:  Terran: Vultures
+Won 71.0280373832% of 107 games
+----------------------
+Matchup:  Zerg: building defense vs Zerg: muta/scourge
+Winner:  Zerg: muta/scourge
+Won 69.3548387097% of 62 games
+----------------------
+Matchup:  Protoss: Cannon rush or Cannon defense + Corsairs vs Zerg: hydra/lurker
+Winner:  Zerg: hydra/lurker
+Won 68.9189189189% of 74 games
+----------------------
+Matchup:  Protoss: zealot rush vs Terran: Vultures
+Winner:  Terran: Vultures
+Won 67.4846625767% of 163 games
+----------------------
+Matchup:  Protoss: Cannon rush or Cannon defense + Corsairs vs Zerg: building defense
+Winner:  Zerg: building defense
+Won 66.3755458515% of 229 games
+----------------------
+Matchup:  Protoss: dragoons, observer for highground vision vs Terran: Marine/Medic combo
+Winner:  Protoss: dragoons, observer for highground vision
+Won 62.0539599652% of 1149 games
+----------------------
+Matchup:  Zerg: muta/scourge vs Zerg: zergling rush
+Winner:  Zerg: muta/scourge
+Won 60.7476635514% of 107 games
+----------------------
+Matchup:  Protoss: zealot rush vs Terran: Marine/Medic combo
+Winner:  Protoss: zealot rush
+Won 59.4285714286% of 175 games
+----------------------
+Matchup:  Protoss: dragoons, observer for highground vision vs Zerg: building defense
+Winner:  Zerg: building defense
+Won 59.1780821918% of 365 games
+----------------------
+Matchup:  Terran: Vultures vs Zerg: building defense
+Winner:  Terran: Vultures
+Won 59.0909090909% of 198 games
+----------------------
+Matchup:  Protoss: dark templar rush vs Zerg: building defense
+Winner:  Zerg: building defense
+Won 58.9743589744% of 117 games
+----------------------
+Matchup:  Protoss: dragoons, observer for highground vision vs Protoss: zealot rush
+Winner:  Protoss: dragoons, observer for highground vision
+Won 56.7849686848% of 479 games
+----------------------
+Matchup:  Protoss: dragoons, observer for highground vision vs Terran: Vultures
+Winner:  Terran: Vultures
+Won 54.7539893617% of 3008 games
+----------------------
+Matchup:  Terran: Marine/Medic combo vs Zerg: zergling rush
+Winner:  Terran: Marine/Medic combo
+Won 53.1930333817% of 1378 games
+
+'''
+
+
+''' how often are strats paired together
+
+race     Strategy Pairs
+Protoss  12                6991
+         13                1114
+         14                 714
+         15                2071
+         23                1865
+         24                1461
+         25                1626
+         34                  46
+         35                  10
+         45                 140
+Terran   12                5369
+         13                1095
+         14                 556
+         15                1217
+         23                1667
+         24                 982
+         25                 865
+         34                  46
+         35                   2
+         45                  74
+Zerg     12                6672
+         13                 977
+         14                 743
+         15                1281
+         23                2044
+         24                1439
+         25                1076
+         34                  30
+         35                   4
+         45                 112
+
+'''
+
+
+'''Gamelength statistics (in minutes)
+race     Strategy
+Protoss  1.0         17.202981
+         2.0         16.055370
+         3.0         13.452830
+         4.0         13.500693
+         5.0         14.154523
+Terran   1.0         15.894536
+         2.0         19.367717
+         3.0         21.297581
+         4.0         17.904709
+Zerg     0.0         11.590912
+         1.0         17.820937
+         2.0         14.818360
+         3.0         12.944106
 '''
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-#zerg 3 strats
-#dropped 'Zerg_Broodling', u'Zerg_Cocoon', 'Zerg_Drone', 'Zerg_Hatchery', 'Zerg_Overlord'
-'''
-zerg3:
-strat1: zergling rush
-Zerg_Zergling         4.299772
-Zerg_Creep_Colony     0.135575
-Zerg_Extractor        0.125312
-Zerg_Spawning_Pool    0.123876
-Zerg_Sunken_Colony    0.085081
-
-strat2: hydra/lurker
-Zerg_Hydralisk       2.484253
-Zerg_Lurker_Egg      0.257773
-Zerg_Lurker          0.206454
-Zerg_Creep_Colony    0.186118
-Zerg_Extractor       0.169311
-
-strat3: muta/scourge
-Zerg_Mutalisk         2.243299
-Zerg_Scourge          0.968641
-Zerg_Creep_Colony     0.226983
-Zerg_Extractor        0.215898
-Zerg_Sunken_Colony    0.191127
-'''
 
 if __name__ == '__main__':
     strat_dict = {'Zerg_0': 'Zerg: zergling rush', 'Zerg_1':'Zerg: building defense', 'Zerg_2': 'Zerg: hydra/lurker', 'Zerg_3': 'Zerg: muta/scourge', 'Terran_1': 'Terran: Marine/Medic combo', 'Terran_2': 'Terran: Vultures', 'Terran_3': 'Terran: siege tanks', 'Terran_4': 'Terran: Goliaths, heavy air defense', 'Protoss_1': 'Protoss: dragoons, observer for highground vision', 'Protoss_2': 'Protoss: zealot rush', 'Protoss_3': 'Protoss: reaver drop', 'Protoss_4': 'Protoss: Cannon rush or Cannon defense + Corsairs', 'Protoss_5': 'Protoss: dark templar rush'}
